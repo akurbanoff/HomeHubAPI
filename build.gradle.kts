0d3005fc72e872ec76710b73e9ba81efe96e842a
Uspesh <Uspeshniy.ae@yandex.ru>
2024-03-03 18:31:39 +0300

    add docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -16,9 +16,9 @@ fun main() {
     val driverClassName = "org.postgresql.Driver"
     val jdbcURL = "jdbc:postgresql://db/${dotenv["DB_NAME"]}"
     val dbConnection = Database.connect(url = jdbcURL, driver = driverClassName, password = dotenv["DB_PASS"], user = dotenv["DB_USER"])
-//    transaction(dbConnection) {
-//        SchemaUtils.create(Client.table, Achieve.table, Employee.table, Object.table, Dealing.table, Company, ClientDescription.table)
-//    }
+    transaction(dbConnection) {
+        SchemaUtils.create(Clients, Achieves, Employees, Objects, Dealings, Company, ClientDescriptions)
+    }
     embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
